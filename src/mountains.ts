bf7e58b56560f0d9858c8724b2a61e9762d31d1f
BobbieHoch <rwhochstadt@gmail.com>
2022-06-27 00:01:07 -0400

    Mountains Tallest

--- src/mountains.ts
+++ src/mountains.ts
@@ -3,16 +3,27 @@ export interface Mountain {
     height: number;
 }
 
-let mountains: Mountain[] = [
+const mountains: Mountain[] = [
     { name: "Kilimanjaro", height: 19341 },
     { name: "Everest", height: 29029 },
     { name: "Denali", height: 20310 },
 ];
+// Declare a function-findNameOfTallestMountain. It takes one parameter, an array of Mountain objects
+// returns string, the name of the tallest mountain. If the array argument is empty, return an empty string ("").
 
-function findNameOfTallestMountain(mountains: Mountain[]) {
-    let highest: Mountain = mountains[0];
-    mountains.forEach((mountain) => {
-        console.log(mountain.height);
+function findNameOfTallestMountain(array: Mountain[]) {
+    let highestMt = array[0];
+    array.forEach((mount) => {
+        if (mount.height > highestMt.height) {
+            highestMt = mount;
+        }
     });
+    return highestMt;
 }
-findNameOfTallestMountain(mountains);
+const tallestPeak = findNameOfTallestMountain(mountains);
+console.log(tallestPeak);
+
+console.log(tallestPeak);
+// Call findNameOfTallestMountain, passing it your mountains array as an argument.
+
+// Store the result of the function call (the return value) in a variable and then console.log the variable. (Hint: It will print “Everest”.)
